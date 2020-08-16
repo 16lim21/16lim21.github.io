d894413bd0938082df833b0f1b3041c7234a9efc
Michael <michael.li@columbia.edu>
2020-08-16 16:32:01 -0400

    Updates

---
+++ precache-manifest.db0bbac0c42dfb78f3120f5cb8668184.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa6a356a56a26c4cc7fbdc3b84a2bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4d65a342550df95f7f7",
+    "url": "/static/css/main.fcdafb19.chunk.css"
+  },
+  {
+    "revision": "dd5c0d4f4d1918d04750",
+    "url": "/static/js/2.37003d52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.37003d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d65a342550df95f7f7",
+    "url": "/static/js/main.a6b49c26.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "1673e7f73aae850ff471e3d1cc5f23d8",
+    "url": "/static/media/BNY.1673e7f7.png"
+  },
+  {
+    "revision": "b6615fc5e01674e0d932bf60545aadb4",
+    "url": "/static/media/Columbia.b6615fc5.png"
+  },
+  {
+    "revision": "fb01e28257907fc7a354c32aa5354489",
+    "url": "/static/media/Spectator-Night.fb01e282.png"
+  },
+  {
+    "revision": "baa8c998648ec17ac5feeed737b2e766",
+    "url": "/static/media/Spectator.baa8c998.png"
+  },
+  {
+    "revision": "4cb0f774c39256a1e9e30843da44b80e",
+    "url": "/static/media/github.4cb0f774.png"
+  },
+  {
+    "revision": "02407f035a1743ee72a6744d095925e3",
+    "url": "/static/media/linkedin.02407f03.png"
+  },
+  {
+    "revision": "f187422ec735574427480c9983018e40",
+    "url": "/static/media/profile.f187422e.JPEG"
+  }
+]);
